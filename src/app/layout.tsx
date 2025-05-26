0bd5a8c05525dc6c46d72bb4e480caf223784dc8
saumyatalwani <saumyaofficial007@gmail.com>
2025-05-26 11:05:11 +0530

    fix: protected layout for navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from '@/context/AuthContext';
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,7 +22,6 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={`${inter.variable} antialiased min-h-screen bg-black`}>
         <AuthProvider>
-          <Navbar/>
           {children}
         </AuthProvider>
       </body>
